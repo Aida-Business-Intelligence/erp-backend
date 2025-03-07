2f7b514d046843b578006487d213a8f1767b266d
Jerônimo Alves Cardoso <jeronimo.alvescardoso@gmail.com>
2025-03-07 13:40:55 -0300

    ajuestes

--- application/models/Cashs_model.php
+++ application/models/Cashs_model.php
@@ -624,11 +624,15 @@ class Cashs_model extends App_Model
         $data['cash_id'] = $detalhes_caixa->id;
 
         $items = isset($data['newitems']) ? $data['newitems'] : [];
+        $data['items'] = json_encode($items);
         unset($data['newitems']);
 
         // Insere a nova entrada de caixa
         $this->db->insert(db_prefix() . 'cashextracts', $data);
         $insert_id = $this->db->insert_id();
+        
+        
+        
 
         if ($insert_id) {
             $update_data = [

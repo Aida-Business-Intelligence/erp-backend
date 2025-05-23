d9bdb657436a5edf63e958a93a8f1e3a882350d4
Jerônimo Alves Cardoso <jeronimo.alvescardoso@gmail.com>
2025-05-23 09:20:29 -0300

    Ajustes .env

--- deploy_gringa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/projetos/erp-backend
-git checkout gringa
-git pull origin gringa
-rsync -av --delete /home/projetos/erp-backend/ /home/gringa/www/
-sudo systemctl restart apache2

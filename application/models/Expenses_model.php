06cb361135276e7b5a20ad9dabfe364e66a83a5d
Lucas Vidal <lucasvidalk9@gmail.com>
2025-07-23 16:27:45 -0300

    adicionando mais um campo de retorno

--- application/models/Expenses_model.php
+++ application/models/Expenses_model.php
@@ -54,6 +54,8 @@ class Expenses_model extends App_Model
 
     public function get_payment_modes()
     {
+        $this->db->select('id, name, description, is_credit_card, is_check, active');
+        $this->db->where('active', 1);
         return $this->db->get(db_prefix() . 'payment_modes')->result_array();
     }
 
